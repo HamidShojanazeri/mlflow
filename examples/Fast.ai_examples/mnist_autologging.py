1c8a5eb6961b8524f5cec5e9e4e2252257cf5833
Hamid Nazeri <Hamid.nazeri2010@gmail.com>
2020-06-12 16:01:16 -0700

    Clean up

--- examples/Fast.ai_examples/mnist_autologging.py
+++ examples/Fast.ai_examples/mnist_autologging.py
@@ -9,26 +9,8 @@ data = ImageDataBunch.from_folder(path)
 model = simple_cnn((3,16,16,2))
 
 
-class MLFlowTracking(LearnerCallback):
-    "A `LearnerCallback` that tracks the loss and other metrics into MLFlow"
-    def __init__(self, learn:Learner):
-        super().__init__(learn)
-        self.learn = learn
-        self.metrics_names = ['train_loss', 'valid_loss'] + [o.__name__ for o in learn.metrics]
-
-    def on_epoch_end(self, epoch, **kwargs:Any)->None:
-        "Send loss and other metrics values to MLFlow after each epoch"
-        if kwargs['smooth_loss'] is None or kwargs["last_metrics"] is None:
-            return
-        metrics = [kwargs['smooth_loss']] + kwargs["last_metrics"]
-        for name, val in zip(self.metrics_names, metrics):
-            mlflow.log_metric(name, np.float(val))
-    def on_train_end(self, **kwargs: Any) -> None:
-        mlflow.fastai.log_model(self.learn, "models")
-
 mlflow.fastai.autolog()
 
-learn = Learner(data, model, metrics=accuracy,
-                   callback_fns=[partial(MLFlowTracking)])
+learn = Learner(data, model, metrics=accuracy)
 
 learn.fit_one_cycle(1)
